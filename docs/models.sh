ec91eefea03272ce02de84690e9f9424a5d75c37
Alex Chen <alexanderchenmh@gmail.com>
2016-12-17 14:57:13 +0800

    udpate

--- docs/models.sh
+++ docs/models.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 
-mix phoenix.gen.html User users name:string age:integer
-mix phoenix.gen.json API.User users name:string age:integer --no-model
-mix phoenix.gen.html Profile profiles agender:string height:decimal
-mix phoenix.gen.json API.Profile profiles agender:string height:decimal --no-model
-mix gen.connect UserToProfile user_to_profiles user_id:integer profile_id:integer
-
-
-
+mix phoenix.gen.html Cat cats name:string age:integer
+mix phoenix.gen.json API.Cat cats name:string age:integer --no-model
+mix phoenix.gen.html Rate rates agender:string height:decimal
+mix phoenix.gen.json API.Rate rates agender:string height:decimal --no-model
+mix gen.connect CatToRate cat_to_rates cat_id:integer rate_id:integer
