44edd0f4fbbaf54bb03dde96dff33d516897482d
Alex Chen <alexanderchenmh@gmail.com>
2016-12-17 15:05:14 +0800

    update

---
+++ docs/connects.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+mix gen.connect CatToRate cat_to_rates cat_id:integer rate_id:integer
--- docs/models.sh
+++ docs/models.sh
@@ -5,4 +5,3 @@ mix phoenix.gen.html Cat cats name:string age:integer
 mix phoenix.gen.json API.Cat cats name:string age:integer --no-model
 mix phoenix.gen.html Rate rates agender:string height:decimal
 mix phoenix.gen.json API.Rate rates agender:string height:decimal --no-model
-mix gen.connect CatToRate cat_to_rates cat_id:integer rate_id:integer
