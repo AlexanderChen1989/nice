931c31d85d7b329930413523be883cd3fb2eb720
Alex Chen <alexanderchenmh@gmail.com>
2016-12-22 02:32:59 +0800

    update

--- lib/gen/tasks/gen.tasks.ex
+++ lib/gen/tasks/gen.tasks.ex
@@ -3,6 +3,20 @@ defmodule Mix.Tasks.Gen.Tasks do
 
   def run(_) do
     Models.tasks
-    |> Enum.map(&IO.inspect/1)
+    |> Enum.map([_, task | args])
+    |> Enum.map(&exec/1)
+  end
+
+  def exec({task, args}) do
+    Mix.shell.info(">>>>>>>>>>>>>>>>>>>>>>>>#{task} #{args |> List.join(" ")}")
+
+    Mix.Task.clear()
+    Mix.Task.run(task, args)
+
+    if "--no-model" in args do
+      sleep 100
+    else
+      sleep 2000
+    end
   end
 end
