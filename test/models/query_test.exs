56c38acf4499f394003ed9064ddedba84445bfd2
Alex Chen <alexanderchenmh@gmail.com>
2016-12-27 15:32:23 +0800

    fix test

--- test/models/query_test.exs
+++ test/models/query_test.exs
@@ -1,5 +1,87 @@
 defmodule Nice.ConnectQuery do
-  use ConnectQuery
+  alias Nice.ConnectQuery
+  alias Nice.{Repo, Owner, Cat, OwnerToCat}
+  alias Ecto.Multi
+  import Ecto.Query
+
+  def owner_add_cat(owner_id, cat_params) do
+    Multi.new
+    |> Multi.run(:owner, &get_owner(&1, owner_id))
+    |> Multi.run(:cat, &create_cat(&1, cat_params))
+    |> Multi.run(:owner_to_cat, &create_owner_to_cat(&1))
+    |> Repo.transaction()
+    |> case do
+      {:ok, changes} -> {:ok, changes.cat}
+      {:error, :cat, changeset, _} -> {:error, changeset.errors}
+      {:error, :owner_to_cat, changeset, _} -> {:error, changeset.errors}
+      {:error, _, reason, _} -> {:error, reason}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  defp create_cat(%{owner: _}, cat_params) do
+    %Cat{}
+    |> Cat.changeset(cat_params)
+    |> Repo.insert
+  end
+
+  defp create_owner_to_cat(%{cat: cat, owner: owner}) do
+    %OwnerToCat{cat: cat, owner: owner}
+    |> Repo.insert
+  end
+
+  defp get_owner(_changes, owner_id) do
+    case Repo.get(Owner, owner_id) do
+      nil -> {:error, "Owner not found"}
+      owner -> {:ok, owner}
+    end
+  end
+
+  defp get_cat(_changes, cat_id) do
+    case Repo.get(Cat, cat_id) do
+      nil -> {:error, "Cat not found"}
+      cat -> {:ok, cat}
+    end
+  end
+
+
+  def owner_connect_cat(owner_id, cat_id) do
+    Multi.new
+    |> Multi.run(:owner, &get_owner(&1, owner_id))
+    |> Multi.run(:cat, &get_cat(&1, cat_id))
+    |> Multi.run(:owner_to_cat, &create_owner_to_cat(&1))
+    |> Repo.transaction()
+    |> case do
+      {:ok, changes} -> {:ok, changes.cat}
+      {:error, :cat, changeset, _} -> {:error, changeset.errors}
+      {:error, :owner_to_cat, changeset, _} -> {:error, changeset.errors}
+      {:error, _, reason, _} -> {:error, reason}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def owner_del_cat(owner_id, cat_id) do
+    query =
+      from owner_to_cat in OwnerToCat,
+        where: [owner_id: ^owner_id, cat_id: ^cat_id]
+
+    {num, _} =  Repo.delete_all(query)
+    {:ok, num}
+  end
+
+  def owner_with_cats(owner_id) do
+    query =
+      from owner in Owner,
+        where: [id: ^owner_id],
+        preload: :cats
+
+    case Repo.one(query) do
+      nil -> {:error, "Owner not found"}
+      owner -> {:ok, owner}
+    end
+  end
+
+
 end
 
 defmodule Nice.OwnerToCatQueryTest do
@@ -27,10 +109,10 @@ defmodule Nice.OwnerToCatQueryTest do
     end
 
     test "fetch o owner by id with cats preloaded", %{owner: owner, cat: cat} do
-      {:ok, _owner} = ConnectQuery.owner_with_cats(owner.id)
-      assert _owner.id == owner.id
-      [_cat] = _owner.cats
-      assert _cat.id == cat.id
+      {:ok, owner_record} = ConnectQuery.owner_with_cats(owner.id)
+      assert owner_record.id == owner.id
+      [cat_record] = owner_record.cats
+      assert cat_record.id == cat.id
     end
 
   end
@@ -58,11 +140,3 @@ defmodule Nice.OwnerToCatQueryTest do
     {:ok, owner_to_cat: owner_to_cat}
   end
 end
-
-
-
-
-
-
-
-
